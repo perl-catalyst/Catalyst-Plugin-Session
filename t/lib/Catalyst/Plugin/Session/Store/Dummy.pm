300eb468569a871e1ef21a990fd4320e7ce457ba
Yuval Kogman <nothingmuch@woobling.org>
2005-10-31 01:52:06 +0000

    live_app.t for Catalyst::Plugin::Session

---
+++ t/lib/Catalyst/Plugin/Session/Store/Dummy.pm
@@ -0,0 +1,46 @@
+#!/usr/bin/perl
+
+package Catalyst::Plugin::Session::Store::Dummy;
+use base qw/Catalyst::Plugin::Session::Store/;
+
+use strict;
+use warnings;
+
+my %store;
+
+sub get_session_data {
+    my ( $c, $sid ) = @_;
+    $store{$sid};
+}
+
+sub store_session_data {
+    my ( $c, $sid, $data ) = @_;
+    $store{$sid} = $data;
+}
+
+sub delete_session_data {
+    my ( $c, $sid ) = @_;
+    delete $store{$sid};
+}
+
+sub delete_expired_sessions { }
+
+__PACKAGE__;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+Catalyst::Plugin::Session::Store::Dummy - 
+
+=head1 SYNOPSIS
+
+	use Catalyst::Plugin::Session::Store::Dummy;
+
+=head1 DESCRIPTION
+
+=cut
+
+
---
+++ t/lib/SessionTestApp.pm
@@ -0,0 +1,36 @@
+#!/usr/bin/perl
+
+package SessionTestApp;
+use Catalyst qw/Session Session::Store::Dummy Session::State::Cookie/;
+
+use strict;
+use warnings;
+
+sub login : Global {
+    my ( $self, $c ) = @_;
+    $c->session;
+    $c->res->output("logged in");
+}
+
+sub logout : Global {
+    my ( $self, $c ) = @_;
+    $c->res->output(
+        "logged out after " . $c->session->{counter} . " requests" );
+    $c->delete_session("logout");
+}
+
+sub page : Global {
+    my ( $self, $c ) = @_;
+    if ( $c->sessionid ) {
+        $c->res->output("you are logged in");
+		$c->session->{counter}++;
+    }
+    else {
+        $c->res->output("please login");
+    }
+}
+
+__PACKAGE__->setup;
+
+__PACKAGE__;
+
