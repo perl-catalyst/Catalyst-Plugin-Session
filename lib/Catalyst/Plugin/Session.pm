ec270ef074c1e4098993fe429850d20928341e45
Yuval Kogman <nothingmuch@woobling.org>
2006-07-31 12:09:44 +0000

    0.08.... *sigh*

--- lib/Catalyst/Plugin/Session.pm
+++ lib/Catalyst/Plugin/Session.pm
@@ -12,7 +12,7 @@ use Digest              ();
 use overload            ();
 use Object::Signature   ();
 
-our $VERSION = "0.07";
+our $VERSION = "0.08";
 
 my @session_data_accessors; # used in delete_session
 BEGIN {
