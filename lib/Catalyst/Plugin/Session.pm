177c24fea9a9957ef78a5d69ea71ee2d770ea60b
Yuval Kogman <nothingmuch@woobling.org>
2006-10-12 18:37:37 +0000

    changelog + version bump C::P::Session

--- lib/Catalyst/Plugin/Session.pm
+++ lib/Catalyst/Plugin/Session.pm
@@ -12,7 +12,7 @@ use Digest              ();
 use overload            ();
 use Object::Signature   ();
 
-our $VERSION = "0.12";
+our $VERSION = "0.13";
 
 my @session_data_accessors; # used in delete_session
 BEGIN {
