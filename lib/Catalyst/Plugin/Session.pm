ad3142b4540d33412549c21314d09257af37f650
Tomas Doran <bobtfish@bobtfish.net>
2012-03-08 16:27:11 +0000

    Version 0.33

--- lib/Catalyst/Plugin/Session.pm
+++ lib/Catalyst/Plugin/Session.pm
@@ -13,7 +13,7 @@ use Carp;
 
 use namespace::clean -except => 'meta';
 
-our $VERSION = '0.32';
+our $VERSION = '0.33';
 $VERSION = eval $VERSION;
 
 my @session_data_accessors; # used in delete_session
@@ -726,6 +726,15 @@ The flash data will be cleaned up only on requests on which actually use
 $c->flash (thus allowing multiple redirections), and the policy is to delete
 all the keys which haven't changed since the flash data was loaded at the end
 of every request.
+
+Note that use of the flash is an easy way to get data across requests, but
+it's also strongly disrecommended, due it it being inherently plagued with
+race conditions. This means that it's unlikely to work well if your
+users have multiple tabs open at once, or if your site does a lot of AJAX
+requests.
+
+L<Catalyst::Plugin::StatusMessage> is the recommended alternative solution,
+as this doesn't suffer from these issues.
 
     sub moose : Local {
         my ( $self, $c ) = @_;
