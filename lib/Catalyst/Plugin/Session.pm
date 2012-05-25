ea548a835d4f0e224179637177274b5dc16c0bf1
Tomas Doran <bobtfish@bobtfish.net>
2012-05-25 14:49:33 +0100

    Revert "Move call to _save_session_expires to finalize_session"
    This reverts commit 94222de2be3cabb5236edfd948be1dea662e7a43.

--- lib/Catalyst/Plugin/Session.pm
+++ lib/Catalyst/Plugin/Session.pm
@@ -102,10 +102,8 @@ sub prepare_action {
 sub finalize_headers {
     my $c = shift;
 
-    # Force extension of session_expires before finalizing headers, so a possible cookie will be
-    # up to date. First call to session_expires will extend the expiry, subsequent calls will
-    # just return the previously extended value.
-    $c->session_expires;
+    # fix cookie before we send headers
+    $c->_save_session_expires;
 
     return $c->maybe::next::method(@_);
 }
@@ -126,7 +124,6 @@ sub finalize_session {
 
     $c->maybe::next::method(@_);
 
-    $c->_save_session_expires;
     $c->_save_session_id;
     $c->_save_session;
     $c->_save_flash;
